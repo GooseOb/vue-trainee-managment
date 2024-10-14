6f47f3e06cae65da8ea7be6712f8789ab27ebe5c
GooseOb <gooose.ob@gmail.com>
2024-10-14 22:56:24 +0200

    fix vite config to make it deployable to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [vue()],
+  base: command === 'build' ? '/vue-trainee-managment/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
